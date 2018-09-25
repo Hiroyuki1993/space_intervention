277cd4dfe701f82529fec42020c83e4986586ca6
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2018-09-25 21:59:20 +0900

    add records

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    records: []
+  },
+  methods: {
+    addRecord: function (tid) {
+      this.records.push({
+        time: new Date().toLocaleTimeString(),
+        table: tid
+      })
+    }
   }
 })
